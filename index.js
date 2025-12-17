dd3b3252acb667d785d7fab826a5a745ffceb4f2
yusuf <yusuf.ali1031@gmail.com>
2025-12-17 13:56:06 +0200

    Completed Flatburger scope lab

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Write your solution in this file!
+const burgers = ["Hamburger", "Cheeseburger"];
+let featuredDrink = "Strawberry Milkshake";
+
+function addBurger() {
+  const newBurger = "Flatburger";
+  burgers.push(newBurger);
+
+  if (true) {
+    const anotherNewBurger = "Maple Bacon Burger";
+    burgers.push(anotherNewBurger);
+  }
+}
+
+function changeFeaturedDrink() {
+  featuredDrink = "The JavaShake";
+}
